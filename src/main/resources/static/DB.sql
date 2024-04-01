f16091a92ea8258b5cbc1729a244bfd85d2b52ce
jstorra <torragarciaj@gmail.com>
2024-04-01 07:10:10 -0500

    feat: :tada: Iniciando proyecto

---
+++ src/main/resources/static/DB.sql
@@ -0,0 +1,52 @@
+DROP DATABASE IF EXISTS audiovisual;
+CREATE DATABASE audiovisual;
+USE audiovisual;
+
+
+CREATE TABLE usuarios(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    dni VARCHAR(255) NOT NULL UNIQUE,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    telefono VARCHAR(255) NOT NULL,
+    contraseña VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tipocontenidos(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE generos(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE plataformas(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE tipocontenido_plataforma (
+    tipo_id INT NOT NULL,
+    plataforma_id INT NOT NULL,
+    PRIMARY KEY (tipo_id, plataforma_id),
+    FOREIGN KEY (tipo_id) REFERENCES tipocontenidos(id),
+    FOREIGN KEY (plataforma_id) REFERENCES plataformas(id)
+);
+
+CREATE TABLE contenidos(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255) NOT NULL,
+    tipo_id INT NOT NULL,
+    genero1_id INT NOT NULL,
+    genero2_id INT,
+    estado ENUM("pendiente", "viendo", "terminado", "abandonado", "en pausa") NOT NULL,
+    plataforma_id INT NOT NULL,
+    calificacion DECIMAL(2,1) NOT NULL,
+    comentario TEXT NOT NULL,
+    FOREIGN KEY (tipo_id) REFERENCES tipocontenidos(id),
+    FOREIGN KEY (genero1_id) REFERENCES generos(id),
+    FOREIGN KEY (genero2_id) REFERENCES generos(id),
+    FOREIGN KEY (plataforma_id) REFERENCES plataformas(id)
+);
---
+++ src/test/java/jstorra/filtro/FiltroApplicationTests.java
@@ -0,0 +1,13 @@
+package jstorra.filtro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
